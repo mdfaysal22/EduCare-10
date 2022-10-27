5087edcf0e398d9257154a14e107ffcc22251778
Md Faysal <mdfaysal.freelancer@gmail.com>
2022-10-27 20:00:15 +0600

    Envirnment Variable Setup

--- src/Components/Pages/Error/Error.js
+++ src/Components/Pages/Error/Error.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
     return (
         <div>
-            <h1>I am Error Page</h1>
+            <div className='text-center py-10'>
+                <h2 className='text-4xl font-semibold'>Page Not Found</h2>
+                <h1 className='text-4xl lg:text-6xl my-4 font-semibold'>4<span className='text-red-400'>0</span>4</h1>
+                <Link to={'/'} className='btn'>Back to Home</Link>
+            </div>
         </div>
     );
 };
--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA9SbIbhfsXAvnpbNDCFdVRqJgh4KqY8tE",
-  authDomain: "educare-a4078.firebaseapp.com",
-  projectId: "educare-a4078",
-  storageBucket: "educare-a4078.appspot.com",
-  messagingSenderId: "262575906993",
-  appId: "1:262575906993:web:ab4b27907a54940de70290"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
